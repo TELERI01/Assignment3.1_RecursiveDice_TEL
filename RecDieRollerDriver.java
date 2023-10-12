e875c04260964adb546fd73ba45b7e960a05a738
TELERI01 <112411816+TELERI01@users.noreply.github.com>
2023-10-12 09:32:21 -0400

    Update RecDieRollerDriver.java

--- RecDieRollerDriver.java
+++ RecDieRollerDriver.java
@@ -1,15 +1,19 @@
 import java.util.Random;
+/**
+ * CS215
+ * @author tylerlericos
+ * Johnson
+ * Recursive Dice
+ */
 public class RecDieRollerDriver {
-	
-	
 	public static void main(String[] args) {
-		
+		//Testing Code
 		System.out.println("====Dice Roller====");
 		System.out.println("i is counting to 1000!");
 		System.out.println("k is counting the 2's rolled!\n");
 		DieRollerDriver dice = new DieRollerDriver();
-		int result = dice.roll(0,0);
-		System.out.println(result);
+		int result = dice.roll(0,0);//Rolls, 0's for base roll
+		System.out.println(result);//Prints the integer result when roll method is finished!
 	}
 
 }
